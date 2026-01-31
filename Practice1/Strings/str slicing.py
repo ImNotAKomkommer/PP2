5d51cd402cfad8df44aef93dff5abb9cd6dc1e68
ImNotAKomkommer <egggorr32@gmail.com>
2026-01-31 09:13:12 +0500

    Add files via upload

---
+++ Practice1/Strings/str slicing.py
@@ -0,0 +1,10 @@
+a = "Hello, World!"
+print(a.upper())
+b = "Hello, World!"
+print(a.lower())
+c = " Hello, World! "
+print(a.strip()) # returns "Hello, World!" 
+d = "Hello, World!"
+print(a.replace("H", "J"))
+e = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!'] 
